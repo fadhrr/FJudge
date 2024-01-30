8b2de98f1164c682307fde05a65a6aa4dbeb3525
riparuk <faruqirifa@gmail.com>
2024-01-30 21:19:58 +0700

    Update

--- app/main.py
+++ app/main.py
@@ -9,6 +9,7 @@ import subprocess
 from fastapi.staticfiles import StaticFiles
 import uvicorn
 from dotenv import load_dotenv
+from app.judger import run_code
 
 load_dotenv()
 
@@ -36,7 +37,7 @@ class CodeExecutionResponse(BaseModel):
 
 
 @app.post("/api/judge")
-async def judger(
+async def judge(
     request: Annotated[
         CodeExecutionRequest,
         Body(
@@ -111,28 +112,4 @@ async def judger(
 
     return results
 
-def run_code(source_code, language, input_data):
-    # Menyimpan source code ke file sementara
-    file_name = "temp_code_executable." +  language
-    with open(file_name, "w") as file:
-        file.write(source_code)
 
-    # Menjalankan source code dengan input tertentu menggunakan subprocess
-    if language == "cpp":
-        result = subprocess.run(["g++", file_name, "-o", "temp_code_executable"], capture_output=True)
-        if result.returncode == 0:
-            result = subprocess.run(["./temp_code_executable"], input=input_data, text=True, capture_output=True)
-        else:
-            raise Exception(result.stderr)
-    elif language == "py":
-        result = subprocess.run(["python", file_name], input=input_data, text=True, capture_output=True)
-    # Tambahkan kondisi untuk bahasa lainnya sesuai kebutuhan
-
-    # Menghapus file sementara
-    subprocess.run(["rm", file_name, "temp_code_executable"])
-
-    # Mengembalikan hasil eksekusi
-    if result.returncode == 0:
-        return result.stdout
-    else:
-        raise Exception(result.stderr)
